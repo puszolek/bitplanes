b79427a696ab4764dab789e03c38e05080b5fd5c
paula <paula@wandlee.com>
2017-01-21 18:23:17 +0100

    init

---
+++ bitplanes/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << "witam" << endl;
+
+
+
+	string x = "";
+
+	cout << "What's your name? ";
+	getline(cin, x);
+
+	return 0;
+}
